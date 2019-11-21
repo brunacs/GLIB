8e3b7b60adb52936d0a231999313f628a64d0d60
Bruna Costa e Silva <bruna@Brunas-MacBook-Pro.local>
2019-11-20 18:34:18 -0800

    add radio button IsActive on page Students

--- queries/3-Student_InsertData.sql
+++ queries/3-Student_InsertData.sql
@@ -1,6 +1,8 @@
+USE GLIB;
+
 -- INSERTING DATA IN A TABLE
-INSERT	INTO STUDENT (FullName, Email, FullAddress, Phone, IsActive)
-		VALUES ('Bruna Costa e Silva', 'brunacs@gmail.com', '', '778-512-1979', 'Y');
+-- INSERT	INTO STUDENT (FullName, Email, FullAddress, Phone, IsActive)
+-- 		VALUES ('Bruna Costa e Silva', 'brunacs@gmail.com', '', '778-512-1979', 'Y');
 
 -- SELECTING / RETRIEVING DATA FROM A TABLE
 SELECT * FROM STUDENT;
